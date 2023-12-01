67fd5b5e326e283cf1f1beeaeb38b44c2d780dec
Nazemrap <corentin.damas@gmail.com>
2023-12-01 12:40:30 +0100

    Get router secure

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Corentin-Damas/chirpy
+
+require github.com/go-chi/chi/v5 v5.0.10
+
+require github.com/go-chi/chi v1.5.5 // indirect
+
+go 1.20
--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
-	fmt.Println("Hello world")
+	const addr = ":8080"
+
+	r := chi.NewRouter()
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
+
+	fgHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	r.Handle("/app", fgHandler)
+	r.Handle("/app/*", fgHandler)
+	r.Get("/healthz", handlerReadiness)
+	r.Get("/metrics", apiCfg.handlerMetrics)
+	r.Get("/reset", apiCfg.handlerReset)
+
+	corsMux := middlewareCors(r)
+
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: corsMux,
+	}
+	fmt.Println("server started on port ")
+	err := srv.ListenAndServe()
+	log.Fatal(err)
 }
